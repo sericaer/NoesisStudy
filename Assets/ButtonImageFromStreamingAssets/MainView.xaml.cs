b7776d5d7c2a8b48bed00716a197a63a514c1098
sericaer <sericaer@outlook.com>
2021-11-10 15:56:19 +0800

    implement button load back image from StreamingAssets

--- Assets/ButtonImageFromStreamingAssets/MainView.xaml.cs
+++ Assets/ButtonImageFromStreamingAssets/MainView.xaml.cs
@@ -1,9 +1,18 @@
 #if UNITY_5_3_OR_NEWER
 #define NOESIS
 using Noesis;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
 #else
 using System;
+using System.Globalization;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 #endif
 
 namespace NoesisStudy.ButtonImageFromStreamingAssets
@@ -24,5 +33,83 @@ namespace NoesisStudy.ButtonImageFromStreamingAssets
             NoesisUnity.LoadComponent(this);
         }
 #endif
+    }
+
+    //public class ImageDetailConverter : IMultiValueConverter
+    //{
+    //    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+    public class ImageDetailConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+#if NOESIS
+            var fileData = File.ReadAllBytes(@"D:\git\NoesisStudy\Assets\StreamingAssets\flower.jpg");
+            var tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData);
+
+            return new TextureSource(tex);
+#else
+            var image = new BitmapImage(new Uri(@"D:\git\NoesisStudy\Assets\StreamingAssets\flower.jpg"));
+            return image;
+#endif
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ImageBrush brush = new ImageBrush();
+            return null;
+        }
+    }
+
+    public class ImageBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(!(value is ImageBrush))
+            {
+                return value;
+            }
+
+#if NOESIS
+            var fileData = File.ReadAllBytes(@"D:\git\NoesisStudy\Assets\StreamingAssets\flower.jpg");
+            var tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData);
+
+            return new ImageBrush(new TextureSource(tex));
+#else
+            var image = new BitmapImage(new Uri(@"D:\git\NoesisStudy\Assets\StreamingAssets\flower.jpg"));
+
+            return new ImageBrush(image);
+#endif
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ImageBrush brush = new ImageBrush();
+            return null;
+        }
+    }
+
+    public class ImageFileLoader : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
